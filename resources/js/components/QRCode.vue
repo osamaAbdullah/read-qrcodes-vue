759f47be63f21a4928bd6ad00ea8c9d8b0512154
osama <osama@aso.com>
2020-04-02 01:47:48 +0300

    now everything is workink perfectly successfully tested and worked

---
+++ resources/js/components/QRCode.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div v-if="read">
+            <qrcode-stream @decode="onDecode"></qrcode-stream>
+            <qrcode-drop-zone></qrcode-drop-zone>
+            <qrcode-capture></qrcode-capture>
+        </div>
+        <div class="btn btn-primary btn-sm" @click=" read = ! read ">{{ read === true ? 'close' : 'open' }}</div>
+    </div>
+</template>
+
+<script>
+
+    import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
+    export default {
+//        components: {
+//            QrcodeStream,
+//            QrcodeDropZone,
+//            QrcodeCapture
+//        },
+        data(){
+          return {
+              read: true,
+          }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            onDecode (decodedString) {
+                console.log(decodedString)
+            }
+        },
+
+    }
+</script>
